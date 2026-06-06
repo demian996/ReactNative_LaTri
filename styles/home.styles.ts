141a6f3dcb071414a921ef5dc48ff2537def6afc
Johao Crespo <johao1999crespo@hotmail.com>
2026-06-06 00:54:07 -0500

    feat: add home screen with Ecuador national team details and logo assets

---
+++ styles/home.styles.ts
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#FFFFFF',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#003DA5',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  logo: {
+    width: 150,
+    height: 150,
+    marginBottom: 20,
+  },
+  info: {
+    fontSize: 18,
+    marginVertical: 5,
+  },
+  buttonContainer: {
+    marginTop: 20,
+  },
+});
